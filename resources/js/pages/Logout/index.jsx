fd79c0980edff2662bc70934c152c242f4b9acfb
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2021-04-25 15:32:39 +0300

    Fix Logout page

--- resources/js/pages/Logout/index.jsx
+++ resources/js/pages/Logout/index.jsx
@@ -1,24 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import classNames from 'classnames';
-import { Container, Form, Button } from 'react-bootstrap';
-import { Redirect } from 'react-router-dom';
-import Header from '../../components/Header';
-import Footer from '../../components/Footer';
+import { Button, Container, Nav } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap';
 import { useUserContext } from '../../context/UserContext';
+import { useSidebarContext } from '../../context/SidebarContext';
 
 const { axios } = window;
 
 const Logout = () => {
-    const { previousPath } = '/';
+    const { isOpen } = useSidebarContext;
     const { userContext, setUserContext } = useUserContext({});
     const { token } = userContext;
-    const [path, setPath] = useState(previousPath);
+    const history = useHistory();
 
-    useEffect(async () => {
-        if (!token) {
-            return;
-        }
-
+    const clearData = async () => {
         const config = {
             method: 'post',
             url: '/api/logout',
@@ -33,49 +29,45 @@ const Logout = () => {
                 // eslint-disable-next-line no-console
                 console.log(JSON.stringify(response.data));
                 setUserContext({});
-                setPath('/login');
             })
             .catch((error) => {
                 // eslint-disable-next-line no-console
                 console.log(error);
             });
-    }, [isLoggedIn]);
+    };
+
+    const handleClick = async (event) => {
+        event.preventDefault();
+        try {
+            await clearData();
+            history.push('/login');
+        } catch (e) {
+            // eslint-disable-next-line no-alert
+            alert(e.message);
+        }
+    };
 
     return (
         <Container
             fluid
-            className={classNames('content', { 'is-open': isOpen })}
+            className={classNames('content', 'text-center', { 'is-open': isOpen })}
         >
-            { token ? <Redirect to="/dashboard" /> : '' }
-            <Header title="Login" />
-            <Form
-                className="w-25 mx-auto mt-5"
-                onSubmit={handleSubmit}
-            >
-                <Form.Label className="mt-3">Email</Form.Label>
-                <Form.Control
-                    name="email"
-                    type="email"
-                    value={userData.email}
-                    onChange={handleChange}
-                />
-                <Form.Label className="mt-3">Password</Form.Label>
-                <Form.Control
-                    name="password"
-                    type="password"
-                    placeholder="Type in new password"
-                    value={userData.password}
-                    onChange={handleChange}
-                />
-                <Button
-                    className="mt-3"
-                    variant="primary"
-                    type="submit"
-                >
-                    Login
-                </Button>
-            </Form>
-            <Footer />
+            <h1>Do you really want to logout?</h1>
+            <div>
+                <Nav className="col-1 justify-content-end" navbar>
+                    <LinkContainer to="" onClick={() => history.goBack()}>
+                        <Button>
+                            Cancel
+                        </Button>
+                    </LinkContainer>
+
+                    <LinkContainer to="/login" onClick={handleClick}>
+                        <Button>
+                            Yes
+                        </Button>
+                    </LinkContainer>
+                </Nav>
+            </div>
         </Container>
     );
 };
