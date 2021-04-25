b47730d78e88af58eb7485867760b5cff27cc024
mariusweb <39734577+mariusweb@users.noreply.github.com>
2021-04-25 13:43:56 +0300

    ESlint format

--- resources/js/components/CreateTaskForm.jsx
+++ resources/js/components/CreateTaskForm.jsx
@@ -1,132 +1,130 @@
-import React, { useState, useEffect } from 'react'
-import { Redirect, useParams } from 'react-router-dom'
-import axios from 'axios'
-import { Form, Button } from 'react-bootstrap'
-import { useUserContext } from '../context/UserContext'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
 
 export default function CreateTaskForm() {
-  // const { userContext, setUserContext } = useUserContext();
-  // const { token } = userContext;
+    // const { userContext, setUserContext } = useUserContext();
+    // const { token } = userContext;
 
-  let { project } = useParams()
-  // console.log(project);
+    const { project } = useParams();
+    // console.log(project);
 
-  const [taskData, setTaskData] = useState({
-    name: '',
-    description: '',
-    priority_id: '',
-    project_id: project,
-  })
+    const [taskData, setTaskData] = useState({
+        name: '',
+        description: '',
+        priority_id: '',
+        project_id: project,
+    });
 
-  const [validationErrors, setErrors] = useState({
-    name: '',
-    description: '',
-    priority_id: '',
-  })
+    const [validationErrors, setErrors] = useState({
+        name: '',
+        description: '',
+        priority_id: '',
+    });
 
-  const [state, setState] = useState(false)
-  const [succesMessage, setSuccesMessage] = useState("")
+    const [state, setState] = useState(false);
+    const [succesMessage, setSuccesMessage] = useState('');
 
-  useEffect(async () => {
-    if (!state) {
-      return
-    }
-    const config = {
-      method: 'post',
-      url: '/api/tasks',
-      // headers: {
-      //     Accept: 'application/json',
-      // },
-      data: taskData,
-    }
+    useEffect(async () => {
+        if (!state) {
+            return;
+        }
+        const config = {
+            method: 'post',
+            url: '/api/tasks',
+            // headers: {
+            //     Accept: 'application/json',
+            // },
+            data: taskData,
+        };
 
-    // console.log(taskData)
+        // console.log(taskData)
 
-    await axios(config)
-      .then((response) => {
-        // eslint-disable-next-line no-console
-        // console.log(response.data)
-        // setUserContext(response.data);
-        setSuccesMessage("Task created succesfully");
-      })
-      .catch((error) => {
-        // eslint-disable-next-line no-console
-        // console.log(error.response.data.errors);
-        setErrors({
-          name: error.response.data.errors.name
-            ? error.response.data.errors.name[0]
-            : '',
-          description: error.response.data.errors.description
-            ? error.response.data.errors.description[0]
-            : '',
-          priority_id: error.response.data.errors.priority_id
-            ? error.response.data.errors.priority_id[0]
-            : '',
-        })
-        // console.log(true);
-      })
-    setState(false)
-  }, [state])
+        await axios(config)
+            .then((response) => {
+                // eslint-disable-next-line no-console
+                // console.log(response.data)
+                // setUserContext(response.data);
+                setSuccesMessage('Task created succesfully');
+            })
+            .catch((error) => {
+                // eslint-disable-next-line no-console
+                // console.log(error.response.data.errors);
+                setErrors({
+                    name: error.response.data.errors.name
+                        ? error.response.data.errors.name[0]
+                        : '',
+                    description: error.response.data.errors.description
+                        ? error.response.data.errors.description[0]
+                        : '',
+                    priority_id: error.response.data.errors.priority_id
+                        ? error.response.data.errors.priority_id[0]
+                        : '',
+                });
+                // console.log(true);
+            });
+        setState(false);
+    }, [state]);
 
-  const handleSubmit = (event) => {
-    event.preventDefault()
-    setSuccesMessage("");
-    setErrors({});
-    // console.log(event);
-    setState(true)
-  }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setSuccesMessage('');
+        setErrors({});
+        // console.log(event);
+        setState(true);
+    };
 
-  const handleChange = (event) =>
-    setTaskData({
-      ...taskData,
-      [event.target.name]: event.target.value,
-    })
+    const handleChange = (event) => setTaskData({
+        ...taskData,
+        [event.target.name]: event.target.value,
+    });
 
-  return (
-    <>
-      {/* {
+    return (
+        <>
+            {/* {
                 token && <Redirect to="/dashboard" />
             } */}
-      <Form className="w-25 mx-auto mt-5 mb-5" onSubmit={handleSubmit}>
-        <Form.Label>Name</Form.Label>
-        <Form.Control
-          name="name"
-          type="text"
-          value={taskData.name}
-          onChange={handleChange}
-        />
-        <div style={{ fontSize: 12 }} className="text-danger">
-          {validationErrors.name}
-        </div>
-        <Form.Label className="mt-3">Description</Form.Label>
-        <Form.Control
-          name="description"
-          as="textarea"
-          rows={5}
-          style={{'resize': 'none'}}
-          value={taskData.description}
-          onChange={handleChange}
-        />
-        <div style={{ fontSize: 12 }} className="text-danger">
-          {validationErrors.description}
-        </div>
-        <Form.Label className="mt-3">Priority</Form.Label>
-        <Form.Control name="priority_id" onChange={handleChange} as="select" custom>
-          <option>--- SELECT PRIORITY ---</option>
-          <option value="1">low</option>
-          <option value="2">medium</option>
-          <option value="3">high</option>
-        </Form.Control>
-        <div style={{ fontSize: 12 }} className="text-danger">
-          {validationErrors.priority_id}
-        </div>
-        <Button className="mt-3" variant="primary" type="submit">
-          Create Task
-        </Button>
-        <div style={{ fontSize: 15 }} className="text-success my-3">
-          {succesMessage}
-        </div>
-      </Form>
-    </>
-  )
+            <Form className="w-25 mx-auto mt-5 mb-5" onSubmit={handleSubmit}>
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                    name="name"
+                    type="text"
+                    value={taskData.name}
+                    onChange={handleChange}
+                />
+                <div style={{ fontSize: 12 }} className="text-danger">
+                    {validationErrors.name}
+                </div>
+                <Form.Label className="mt-3">Description</Form.Label>
+                <Form.Control
+                    name="description"
+                    as="textarea"
+                    rows={5}
+                    style={{ resize: 'none' }}
+                    value={taskData.description}
+                    onChange={handleChange}
+                />
+                <div style={{ fontSize: 12 }} className="text-danger">
+                    {validationErrors.description}
+                </div>
+                <Form.Label className="mt-3">Priority</Form.Label>
+                <Form.Control name="priority_id" onChange={handleChange} as="select" custom>
+                    <option>--- SELECT PRIORITY ---</option>
+                    <option value="1">low</option>
+                    <option value="2">medium</option>
+                    <option value="3">high</option>
+                </Form.Control>
+                <div style={{ fontSize: 12 }} className="text-danger">
+                    {validationErrors.priority_id}
+                </div>
+                <Button className="mt-3" variant="primary" type="submit">
+                    Create Task
+                </Button>
+                <div style={{ fontSize: 15 }} className="text-success my-3">
+                    {succesMessage}
+                </div>
+            </Form>
+        </>
+    );
 }
