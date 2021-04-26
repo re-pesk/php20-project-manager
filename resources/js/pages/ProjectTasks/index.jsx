78e807bbd3f2ce476e1cde716d0862ff1e235d9e
mariusweb <39734577+mariusweb@users.noreply.github.com>
2021-04-26 22:21:42 +0300

    Tasks.jsx ajustments

--- resources/js/pages/ProjectTasks/index.jsx
+++ resources/js/pages/ProjectTasks/index.jsx
@@ -1,13 +1,8 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
 import Tasks from './Tasks/Tasks';
 
 export default function ProjectTasks() {
     return (
-        <Container>
-
-            <Tasks />
-
-        </Container>
+        <Tasks />
     );
 }
