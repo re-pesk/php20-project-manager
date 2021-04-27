c56d635a96bd3385a445612be36a370ba81fd562
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2021-04-27 16:58:18 +0300

    Fix typo

--- resources/js/components/RouteList/CheckedRoute.jsx
+++ resources/js/components/RouteList/CheckedRoute.jsx
@@ -3,7 +3,7 @@ import { Redirect, Route } from 'react-router-dom';
 import { useUserContext } from '../../context/UserContext';
 import { accessType } from './data/route-list-data';
 
-export default function PublicOnlyRoute({ accessibility, children, ...rest }) {
+export default function CheckedRoute({ accessibility, children, ...rest }) {
     const { userContext } = useUserContext();
     const { user } = userContext;
     const canAccess = (
