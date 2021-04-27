646dd70dff9428aae7a97dcc7534ea82a5748206
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2021-04-27 17:36:04 +0300

    Format according ESLint rules

--- resources/js/components/RouteList/CheckedRoute.jsx
+++ resources/js/components/RouteList/CheckedRoute.jsx
@@ -3,7 +3,7 @@ import { Redirect, Route } from 'react-router-dom';
 import { useUserContext } from '../../context/UserContext';
 import { accessType } from './data/route-list-data';
 
-export default function CheckedRoute({ accessibility, children, ...rest }) {
+export default function CheckedRoute({ accessibility, children, exact, key, path }) {
     const { userContext } = useUserContext();
     const { user } = userContext;
     const canAccess = (
@@ -14,8 +14,9 @@ export default function CheckedRoute({ accessibility, children, ...rest }) {
 
     return (
         <Route
-            // eslint-disable-next-line react/jsx-props-no-spreading
-            {...rest}
+            key={key}
+            exact={exact}
+            path={path}
             render={({ location }) => (canAccess ? (
                 children
             ) : (
