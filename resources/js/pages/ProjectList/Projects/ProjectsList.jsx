384f9238d87487662692caeb6c85dc8e18bc6695
Karolis Kvatavicius <karolis.kvatavicius@gmail.com>
2021-04-29 15:21:17 +0300

    Added show board link to projects list

--- resources/js/pages/ProjectList/Projects/ProjectsList.jsx
+++ resources/js/pages/ProjectList/Projects/ProjectsList.jsx
@@ -131,8 +131,20 @@ const Projects = () => {
                                                 }}
                                             >
                                                 View tasks
-                                        </Button>
-                                            <Button className="mr-1">Show Board</Button>
+                                            </Button>
+                                            <Button
+                                                className="mr-1"
+                                                onClick={() => {
+                                                    history.push({ pathname: '/project/board',
+                                                        state: {
+                                                            project: project.id,
+                                                            task: null,
+                                                        } });
+                                                }}
+                                            >
+                                                Show Board
+
+                                            </Button>
                                             <Button
                                                 className="mr-1"
                                                 type="submit"
