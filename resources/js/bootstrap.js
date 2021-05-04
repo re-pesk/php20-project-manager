a6ba930c91a698b05a4a60f0606b0f4edfcdd11d
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2021-05-04 22:03:32 +0300

    Update

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -10,9 +10,8 @@ window._ = require('lodash');
 window.axios = require('axios');
 
 window.axios.defaults.withCredentials = true;
-/*
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
+/*
 const csrfToken = document.head.querySelector('meta[name="csrf-token"]');
 
 if (csrfToken) {
