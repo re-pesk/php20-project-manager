51784e4a4135045157acf6bd55f1b97923257ec9
Karolis Kvatavicius <karolis.kvatavicius@gmail.com>
2021-05-08 09:51:56 +0300

    Prettier validation for create project form

--- resources/js/pages/CreateProject/CreateProjectForm.jsx
+++ resources/js/pages/CreateProject/CreateProjectForm.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import { Form, Button } from 'react-bootstrap';
 
@@ -10,8 +9,8 @@ export default function CreateProjectForm() {
     });
 
     const [validationErrors, setErrors] = useState({
-        name: '',
-        description: '',
+        name: '-',
+        description: '-',
     });
 
     const [state, setState] = useState(false);
@@ -24,24 +23,18 @@ export default function CreateProjectForm() {
         const config = {
             method: 'post',
             url: '/api/projects',
-            // headers: {
-            //     Accept: 'application/json',
-            // },
             data: projectData,
         };
 
-        // console.log(taskData)
-
         await axios(config)
             .then(() => {
-                // eslint-disable-next-line no-console
-                // console.log(response.data)
-                // setUserContext(response.data);
+                setErrors({
+                    name: '-',
+                    description: '-',
+                });
                 setSuccesMessage('Project created succesfully');
             })
             .catch((error) => {
-                // eslint-disable-next-line no-console
-                // console.log(error.response.data.errors);
                 setErrors({
                     name: error.response.data.errors.name
                         ? error.response.data.errors.name[0]
@@ -50,7 +43,6 @@ export default function CreateProjectForm() {
                         ? error.response.data.errors.description[0]
                         : '',
                 });
-                // console.log(true);
             });
         setState(false);
     }, [state]);
@@ -58,8 +50,6 @@ export default function CreateProjectForm() {
     const handleSubmit = (event) => {
         event.preventDefault();
         setSuccesMessage('');
-        setErrors({});
-        // console.log(event);
         setState(true);
     };
 
@@ -70,39 +60,33 @@ export default function CreateProjectForm() {
 
     return (
         <>
-            {/* {
-                token && <Redirect to="/dashboard" />
-            } */}
-            <Form className="w-25 mx-auto mt-5 mb-5" onSubmit={handleSubmit}>
-                <Form.Label>Name</Form.Label>
-                <Form.Control
-                    name="name"
-                    type="text"
-                    value={projectData.name}
-                    onChange={handleChange}
-                />
-                <div style={{ fontSize: 12 }} className="text-danger">
-                    {validationErrors.name}
-                </div>
-                <Form.Label className="mt-3">Description</Form.Label>
-                <Form.Control
-                    name="description"
-                    as="textarea"
-                    rows={5}
-                    style={{ resize: 'none' }}
-                    value={projectData.description}
-                    onChange={handleChange}
-                />
-                <div style={{ fontSize: 12 }} className="text-danger">
-                    {validationErrors.description}
-                </div>
-                {/* <Form.Label className="mt-3">Project State</Form.Label>
-                <Form.Control name="project_state_id" onChange={handleChange} as="select" custom>
-                    <option value="1">In Progress</option>
-                </Form.Control>
-                <div style={{ fontSize: 12 }} className="text-danger">
-                    {validationErrors.project_state_id}
-                </div> */}
+            <Form noValidate className="w-25 mx-auto mt-5 mb-5" onSubmit={handleSubmit}>
+                <Form.Group controlId="name">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control
+                        name="name"
+                        type="text"
+                        value={projectData.name}
+                        onChange={handleChange}
+                        isValid={Boolean(validationErrors.name === '')}
+                        isInvalid={Boolean(validationErrors.name !== '' && validationErrors.name !== '-')}
+                    />
+                    <Form.Control.Feedback type="invalid">{validationErrors.name}</Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group controlId="description">
+                    <Form.Label className="mt-3">Description</Form.Label>
+                    <Form.Control
+                        name="description"
+                        as="textarea"
+                        rows={5}
+                        style={{ resize: 'none' }}
+                        value={projectData.description}
+                        onChange={handleChange}
+                        isValid={Boolean(validationErrors.description === '')}
+                        isInvalid={Boolean(validationErrors.description !== '' && validationErrors.description !== '-')}
+                    />
+                    <Form.Control.Feedback type="invalid">{validationErrors.description}</Form.Control.Feedback>
+                </Form.Group>
                 <Button className="mt-3" variant="primary" type="submit">
                     Create Project
                 </Button>
