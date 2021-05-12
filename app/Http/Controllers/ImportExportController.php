d78bcc863acbe7d3b5eee0615a4f324497582c58
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2021-05-13 00:54:11 +0300

    Add export controller

--- app/Http/Controllers/ImportExportController.php
+++ app/Http/Controllers/ImportExportController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Maatwebsite\Excel\Facades\Excel;
-use App\Exports\DataExport;
+use App\Exports\ProjectsExport;
+use App\Exports\TasksExport;
+use Maatwebsite\Excel\Excel;
 
 class ImportExportController extends Controller
 {
@@ -15,10 +16,38 @@ class ImportExportController extends Controller
         $this->middleware(['log.routes']);
     }
 
-    public function export()
+    public function exportProjectList()
     {
-        return Excel::download(new DataExport, 'users-list.xlsx');
+        $fileName = 'project-list.csv';
+
+        return (new ProjectsExport)->download($fileName, Excel::CSV);
+    }
+
+    public function exportProject($project)
+    {
+        $fileName = 'project-' . $project . '.csv';
+        return (new ProjectsExport)->whereId($project)->download($fileName, Excel::CSV);
+    }
+
+    public function exportProjectTaskList($project)
+    {
+        $fileName = 'project-' . $project . '-task-list.csv';
+        return (new ProjectsExport)->whereId($project)->taskList()->download($fileName, Excel::CSV);
         // Excel::store(new DataExport, 'users-list.xlsx');
         // return response()->file(storage_path().'/exports/Filename2.xlsx');
+    }
+
+    public function exportTaskList()
+    {
+        $fileName = 'task-list.csv';
+
+        return (new TasksExport)->download($fileName, Excel::CSV);
+    }
+
+    public function exportTask($task)
+    {
+        $fileName = 'task-'. $task .'.csv';
+
+        return (new TasksExport)->whereId($task)->download($fileName, Excel::CSV);
     }
 }
