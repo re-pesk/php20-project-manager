c7d19931c138a197b490460e644dc5089f449f7f
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2021-05-13 00:49:12 +0300

    Add routes

--- routes/api.php
+++ routes/api.php
@@ -33,7 +33,12 @@ return $request->user();
 
 Route::post('/logged-in', [AuthController::class, 'isLoggedIn']);
 Route::post('/log', [LogController::class, 'addToLog']);
-Route::get('/data/export', [ImportExportController::class, 'export'])->name('data.export');
+
+Route::get('/projects/export', [ImportExportController::class, 'exportProjectList'])->name('project.list.export');
+Route::get('/projects/{project}/export', [ImportExportController::class, 'exportProject'])->name('project.export');
+Route::get('/projects/{project}/tasks/export', [ImportExportController::class, 'exportProjectTaskList'])->name('project.task.list.export');
+Route::get('/tasks/export', [ImportExportController::class, 'exportTaskList'])->name('task.list.export');
+Route::get('/tasks/{task}/export', [ImportExportController::class, 'exportTask'])->name('task.export');
 
 
 Route::apiResource('projects', ProjectController::class);
