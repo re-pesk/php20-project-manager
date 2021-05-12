8892ecb3671ef051d09501cf5908b76b425b018e
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2021-05-13 01:39:38 +0300

    Generate download buttons from data

--- resources/js/components/Buttons.jsx
+++ resources/js/components/Buttons.jsx
@@ -2,14 +2,21 @@ import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import DownloadButton from './DownloadButton';
 
+const buttonData = [
+    { endpoint: 'api/projects/export', title: 'Project List' },
+    { endpoint: 'api/projects/1/export', title: 'Project #1' },
+    { endpoint: 'api/projects/1/tasks/export', title: 'Project #1 Task List' },
+    { endpoint: 'api/tasks/export', title: 'Task List' },
+    { endpoint: 'api/tasks/1/export', title: 'Task #1' },
+];
+
 const Buttons = () => (
     <Container>
         <Row className="text-center">
-            <Col><DownloadButton endpoint="api/projects/export" title="Project List" /></Col>
-            <Col><DownloadButton endpoint="api/projects/1/export" title="Project #1" /></Col>
-            <Col><DownloadButton endpoint="api/projects/1/tasks/export" title="Project #1 Task List" /></Col>
-            <Col><DownloadButton endpoint="api/tasks/export" title="Task List" /></Col>
-            <Col><DownloadButton endpoint="api/tasks/1/export" title="Task #1" /></Col>
+            {buttonData.map((item, index) => {
+                const key = `btn-${index}`;
+                return (<Col><DownloadButton key={key} endpoint={item.endpoint} title={item.title} /></Col>);
+            })}
         </Row>
     </Container>
 );
