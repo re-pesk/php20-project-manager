6d6588d35e5ff156f40bdd79601ba7f97ce683f4
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2021-05-13 17:46:15 +0300

    Fix dashboard

--- resources/js/components/Buttons.jsx
+++ resources/js/components/Buttons.jsx
@@ -6,7 +6,7 @@ const buttonData = [
     { endpoint: 'api/export/projects', title: 'Project List' },
     { endpoint: 'api/export/projects/1', title: 'Project #1' },
     { endpoint: 'api/export/projects/1/tasks', title: 'Project #1 Task List' },
-    { endpoint: 'api/export/tasks', title: 'Task List' },
+    // { endpoint: 'api/export/tasks', title: 'Task List' },
     { endpoint: 'api/export/tasks/1', title: 'Task #1' },
 ];
 
