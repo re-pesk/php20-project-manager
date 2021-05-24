d73c34761bcfb7669394333003a55196f1538d20
Karolis Kvatavicius <karolis.kvatavicius@gmail.com>
2021-05-24 14:53:18 +0300

    improved sidebar, fixed window scroll issues

--- resources/js/components/Sidebar/NavItem.jsx
+++ resources/js/components/Sidebar/NavItem.jsx
@@ -3,13 +3,15 @@ import React from 'react';
 import { Nav } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useSidebarContext } from '../../context/SidebarContext';
 
 const NavItem = (props) => {
     const { href, icon, children } = props;
+    const { isOpen, setSidebarContext } = useSidebarContext();
 
     return (
         <Nav.Item className={classNames({ active: (href === window.location.pathname) })}>
-            <LinkContainer to={href}>
+            <LinkContainer to={href} onClick={() => { setSidebarContext(!isOpen); }}>
                 <Nav.Link>
                     <FontAwesomeIcon icon={icon} className="mr-2" />
                     {children}
