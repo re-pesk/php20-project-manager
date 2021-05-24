93e06a34cf973e33c8a88b1731e6deca8164484b
Zilvinas <zilkazakauskas@gmail.com>
2021-05-24 21:02:58 +0300

    dashboard v1

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,24 +6,34 @@ use App\Models\Project;
 use App\Models\Task;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Doctrine\Common\Inflector\Inflector;
+
+
 
 class DashboardController extends Controller
 {
     public function __construct()
     {
         $this->middleware(['cors']);
-        // $this->middleware(['auth:sanctum']);
+        $this->middleware(['auth:sanctum']);
         $this->middleware(['log.routes']);
     }
 
     public function dashboardData(){
+        $projectCount = Project::get()->count();
+        $finishedProjectCount = Project::where('project_state_id', '2')->count();
+        $projectsCreatedLastWeekCount = Project::whereBetween('created_at', [Carbon::now()->startOfWeek(), Carbon::now()->endOfWeek()])->count();
+        $taskCount = Task::get()->count();
+        $finishedTaskCount = Task::where('task_state_id', '3')->count();
+        $tasksCreatedLastWeekCount = Task::whereBetween('created_at', [Carbon::now()->startOfWeek(), Carbon::now()->endOfWeek()])->count();
         $allData = array(
-            'projectCount' => Project::get()->count(),
-            'finishedProjectCount' => Project::where('project_state_id', '2')->count(),
-            'projectsCreatedLastWeekCount' => Project::whereBetween('created_at', [Carbon::now()->startOfWeek(), Carbon::now()->endOfWeek()])->count(),
-            'taskCount' => Task::get()->count(),
-            'finishedTaskCount' => Task::where('task_state_id', '3')->count(),
-            'tasksCreatedLastWeekCount' => Task::whereBetween('created_at', [Carbon::now()->startOfWeek(), Carbon::now()->endOfWeek()])->count(),
+            'projectCount' => $projectCount." ".Str::plural('project', $projectCount),
+            'finishedProjectCount' => $finishedProjectCount. " finished ".Str::plural('project', $finishedProjectCount),
+            'projectsCreatedLastWeekCount' => $projectsCreatedLastWeekCount." ".Str::plural('project', $projectsCreatedLastWeekCount),
+            'taskCount' => $taskCount." ".Str::plural('task', $taskCount),
+            'finishedTaskCount' => $finishedTaskCount." finished ".Str::plural('task', $finishedTaskCount),
+            'tasksCreatedLastWeekCount' => $tasksCreatedLastWeekCount." ".Str::plural('task', $tasksCreatedLastWeekCount),
         );
         return $allData;
     }
--- resources/js/pages/Dashboard/index.jsx
+++ resources/js/pages/Dashboard/index.jsx
@@ -1,35 +1,34 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { Button, Container } from 'react-bootstrap';
+import { Button, Card, Container, ListGroup, Spinner } from 'react-bootstrap';
 import { useHistory } from 'react-router-dom';
 
 export default function Dashboard() {
     const history = useHistory();
 
-    // const [projectsCount, setProjectCount] = useState([]);
+    const [projectData, setProjectData] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-    // useEffect(async () => {
-    //     const config = {
-    //         method: 'GET',
-    //         url: `/api/search-projects/${searchVar}?page=${currentPage}`,
-    //         headers: {
-    //             Accept: 'application/json',
-    //         },
-    //     };
+    useEffect(async () => {
+        const config = {
+            method: 'GET',
+            url: '/api/dashboard',
+            headers: {
+                Accept: 'application/json',
+            },
+        };
 
-    //     await axios(config)
-    //         .then((response) => {
-    //             // console.log(response.data);
-    //             // setProjectsData(response.data);
-    //             // setProjectsData(response.data.data);
-    //             // setLastPage(response.data.last_page);
-    //             // setLoading(false);
-    //         })
-    //         .catch((error) => {
-    //             console.log(error);
-    //         });
-    //     // console.log(searchVar);
-    // }, []);
+        await axios(config)
+            .then((response) => {
+                setProjectData(response.data);
+                setLoading(false);
+            })
+            .catch((error) => {
+                // eslint-disable-next-line no-console
+                console.log(error);
+            });
+        // console.log(searchVar);
+    }, []);
 
     return (
         <Container>
@@ -45,6 +44,94 @@ export default function Dashboard() {
                     Back
                 </Button>
             </div>
+            <div className="row">
+                <div className="col">
+                    <h1 className="mt-5 text-center">Database statistics</h1>
+                    <div className="text-center">
+                        <img src="../../img/chart.png" alt="chart" />
+                    </div>
+                </div>
+            </div>
+            {loading === true ? (
+                <div className="row">
+                    <div className="col">
+                        <div className="mt-2 font-weight-bold text-center">
+                            Loading data...
+                            <Spinner animation="border" variant="primary" className="ml-2" />
+                        </div>
+                    </div>
+                </div>
+            )
+                : (
+                    <div className="row">
+                        <div className="col-lg-6 col-sm-12 mt-5">
+                            <Card>
+                                <ListGroup variant="flush">
+                                    <ListGroup.Item>
+                                        There
+                                        {' '}
+                                        {projectData.projectCount === '1 project' ? ('is') : ('are')}
+                                        {' '}
+                                        <strong>{projectData.projectCount}</strong>
+                                        {' '}
+                                        in the database.
+                                    </ListGroup.Item>
+                                    <ListGroup.Item>
+                                        There
+                                        {' '}
+                                        {projectData.finishedProjectCount === '1 finished project' ? ('is') : ('are')}
+                                        {' '}
+                                        <strong>{projectData.finishedProjectCount}</strong>
+                                        {' '}
+                                        in the database.
+                                    </ListGroup.Item>
+                                    <ListGroup.Item>
+                                        There
+                                        {' '}
+                                        {projectData.projectsCreatedLastWeekCount === '1 project' ? ('is') : ('are')}
+                                        {' '}
+                                        <strong>{projectData.projectsCreatedLastWeekCount}</strong>
+                                        {' '}
+                                        in the database that has been created this week.
+                                    </ListGroup.Item>
+                                </ListGroup>
+                            </Card>
+                        </div>
+                        <div className="col-lg-6 col-sm-12 mt-5">
+                            <Card>
+                                <ListGroup variant="flush">
+                                    <ListGroup.Item>
+                                        There
+                                        {' '}
+                                        {projectData.projectCount === '1 task' ? ('is') : ('are')}
+                                        {' '}
+                                        <strong>{projectData.taskCount}</strong>
+                                        {' '}
+                                        in the database.
+                                    </ListGroup.Item>
+                                    <ListGroup.Item>
+                                        There
+                                        {' '}
+                                        {projectData.finishedTaskCount === '1 finished task' ? ('is') : ('are')}
+                                        {' '}
+                                        <strong>{projectData.finishedTaskCount}</strong>
+                                        {' '}
+                                        in the database.
+                                    </ListGroup.Item>
+                                    <ListGroup.Item>
+                                        There
+                                        {' '}
+                                        {projectData.tasksCreatedLastWeekCount === '1 task' ? ('is') : ('are')}
+                                        {' '}
+                                        <strong>{projectData.tasksCreatedLastWeekCount}</strong>
+                                        {' '}
+                                        in the database that has been created this week.
+                                    </ListGroup.Item>
+                                </ListGroup>
+                            </Card>
+                        </div>
+                    </div>
+                )}
         </Container>
 
     );
