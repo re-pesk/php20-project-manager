123d0c9c8d5a00a9fcd57330fabaea3467aa0bb9
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2021-06-21 12:40:52 +0300

    Create production public folder

---
+++ public/js/228.js
@@ -0,0 +1,1 @@
+(self.webpackChunkphprojectman=self.webpackChunkphprojectman||[]).push([[228],{1228:(e,n,a)=>{"use strict";a.r(n),a.d(n,{default:()=>p});a(7294);var r=a(1592),c=a(5893);const p=function(e){var n=e.emailKey;return(0,c.jsx)("span",{children:r.IC[n]})}}}]);
