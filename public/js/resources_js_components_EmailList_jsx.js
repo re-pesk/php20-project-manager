c6eae6dde464e75dea79760c4062e887ca92e6f1
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2021-06-21 04:10:24 +0300

    Update public folder

---
+++ public/js/resources_js_components_EmailList_jsx.js
@@ -0,0 +1,40 @@
+(self["webpackChunkphprojectman"] = self["webpackChunkphprojectman"] || []).push([["resources_js_components_EmailList_jsx"],{
+
+/***/ "./resources/js/components/EmailList.jsx":
+/*!***********************************************!*\
+  !*** ./resources/js/components/EmailList.jsx ***!
+  \***********************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
+
+
+var emailList = {
+  akvile: 'akvile (dot) gurskaite (at) gmail (dot) com',
+  arnoldas: 'arnoldas (dot) venckus (at) gmail (dot) com',
+  julius: 'julius (dot) cincikas (at) gmail (dot) com',
+  karolis: 'karolis (dot) kvatavicius (at) gmail (dot) com',
+  marius: 'marius (dot) serys (at) gmail (dot) com',
+  redas: 'redas (dot) peskaitis (at) gmail (dot) com',
+  zilvinas: 'zil (dot) kazakauskas (at) gmail (dot) com'
+};
+
+var ContactEmail = function ContactEmail(_ref) {
+  var name = _ref.name;
+  return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("span", {
+    children: emailList[name]
+  });
+};
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (ContactEmail);
+
+/***/ })
+
+}]);
+//# sourceMappingURL=resources_js_components_EmailList_jsx.js.map
