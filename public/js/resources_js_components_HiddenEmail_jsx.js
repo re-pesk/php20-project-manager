83c7e739f62d6c281a61a921225a7d96a95f86b0
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2021-06-21 04:12:37 +0300

    Make production version

---
+++ public/js/resources_js_components_HiddenEmail_jsx.js
@@ -0,0 +1,40 @@
+(self["webpackChunkphprojectman"] = self["webpackChunkphprojectman"] || []).push([["resources_js_components_HiddenEmail_jsx"],{
+
+/***/ "./resources/js/components/HiddenEmail.jsx":
+/*!*************************************************!*\
+  !*** ./resources/js/components/HiddenEmail.jsx ***!
+  \*************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
+
+
+var emailList = {
+  akvile: 'akvile.gurskaite@gmail.com',
+  arnoldas: 'arnoldas.venckus@gmail.com',
+  julius: 'julius.cincikas@gmail.com',
+  karolis: 'karolis.kvatavicius@gmail.com',
+  marius: 'marius.serys@gmail.com',
+  redas: 'redas.peskaitis@gmail.com',
+  zilvinas: 'zil.kazakauskas@gmail.com'
+};
+
+var HiddenEmail = function HiddenEmail(_ref) {
+  var emailKey = _ref.emailKey;
+  return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("span", {
+    children: emailList[emailKey]
+  });
+};
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (HiddenEmail);
+
+/***/ })
+
+}]);
+//# sourceMappingURL=resources_js_components_HiddenEmail_jsx.js.map
